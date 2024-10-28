2d7f3f43a8c9e38ea8bc993f36f13197d817f930
Satish Hiremath <satishmhiremath@gmail.com>
2024-10-28 22:18:49 +0530

    code changes for factory

--- DesignPatterns/src/com/test/creational/factory/Client.java
+++ DesignPatterns/src/com/test/creational/factory/Client.java
@@ -1,11 +1,21 @@
 package com.test.creational.factory;
 
+import java.time.LocalDateTime;
+
 public class Client {
 	public static void main(String[] args) {
 		Post post = new PostFactory().createPost("blog");
 		if(post instanceof BlogPost) {
 			((BlogPost)post).setAuthor("A");
 			((BlogPost) post).setTags(new String[] {"Java", "Design Patterns", "Factory"});
+		}
+		if(post instanceof NewsPost) {
+			((NewsPost)post).setHeadlines("");
+			((NewsPost)post).setNewsTime(LocalDateTime.now());
+		}
+		if(post instanceof ProductPost) {
+			((ProductPost)post).setName("A");
+			((ProductPost)post).setImageUrl("https://.......");
 		}
 		System.out.println(post);
 	}
